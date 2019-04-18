aac453f3bb982aec97f24d5eec6fedb4a4501cd5
poholo502 <poholo502@gmail.com>
2019-04-18 17:04:26 -0500

    RegisterFormStarted

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  public requestSubmitted: boolean = false;
+  public registerGroup: FormGroup = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    username: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl('')
+  });
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  Register() {
+    this.requestSubmitted = true;
+    console.log(this.registerGroup.valid);
+    if(this.registerGroup.invalid){
+      return;
+    }
+    let newUSer: String = JSON.stringify(this.registerGroup.value);
+    
+    console.log(newUSer);
   }
 
 }
