35e51ea32d437d87e514712f61e9f844e04f2e71
poholo502 <poholo502@gmail.com>
2019-04-25 16:59:46 -0500

    Commit before pull

--- src/app/edit-info/edit-info.component.ts
+++ src/app/edit-info/edit-info.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DatabaseService } from '../database.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-edit-info',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditInfoComponent implements OnInit {
 
-  constructor() { }
+  public editSubmitted: boolean = false;
+  public editGroup: FormGroup = new FormGroup({
+    first_name: new FormControl(''),
+    last_name: new FormControl(''),
+    email: new FormControl(''),
+    username: new FormControl('')
+  });
+
+  public data: Object = {
+  
+  }
+
+  constructor(private router: Router, private _db: DatabaseService, fb: FormBuilder) { 
+    //this is from a tutorial, they put this in the constructor, don't know why
+    this.editGroup = fb.group({
+      first_name: [], //call first_name getter
+      last_name: [], // call last_name getter
+      email: [],  // call email getter
+      username: [] // call username getter
+    });
+  }
 
   ngOnInit() {
+  }
+
+  Update(){
+    console.log(this.editGroup.valid);
+    if (this.editGroup.invalid) {
+      return;
+    }
+
+    // call setter for first_name
+    // call setter for last_name
+    // call setter for email
+    // call setter for username
+  }
+
+  Cancel(){
+    this.router.navigateByUrl('account');
   }
 
 }
